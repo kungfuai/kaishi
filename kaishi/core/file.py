de1bcdc050468f7aa553830928c4e21f7ec27274
Michael Wharton <michael@kungfu.ai>
2020-03-05 12:27:42 -0600

    Add support for string labels

--- kaishi/core/file.py
+++ kaishi/core/file.py
@@ -1,6 +1,9 @@
 """Class definition for reading/writing files of various types."""
 import os
+from kaishi.core.labels import Labels
+from kaishi.core.misc import is_valid_label
 from kaishi.core.misc import md5sum
+import warnings
 
 
 class File:
@@ -33,19 +36,39 @@ class File:
 
         return self.hash
 
-    def add_label(self, label):
+    def has_label(self, label_to_check: str):
+        """Check if file has a specific label."""
+        if not is_valid_label(label_to_check, Labels):
+            warnings.warn(
+                "Check for label "
+                + label_to_check
+                + " returns False because label is not valid"
+            )
+            return False
+        return bool(label_to_check in [label.name for label in self.labels])
+
+    def add_label(self, label_to_add: str):
         """Add a label to a file object."""
-        if label not in self.labels:
-            self.labels.append(label)
+        if not is_valid_label(label_to_add, Labels):
+            raise ValueError(
+                "Cannot add label " + str(label_to_add) + " as it is not valid"
+            )
+        if label_to_add not in self.labels:
+            self.labels.append(Labels[label_to_add])
 
         # Ensure the list is always sorted
         string_list = [label.name for label in self.labels]
         sort_ind = sorted(range(len(string_list)), key=lambda k: string_list[k])
         self.labels = [self.labels[i] for i in sort_ind]
 
-    def remove_label(self, label):
+    def remove_label(self, label_to_remove: str):
         """Remove a label from a file object."""
+        if not is_valid_label(label_to_remove, Labels):
+            warnings.warn(
+                "Can't remove " + label_to_remove + " as it is not a valid label"
+            )
+            return
         try:
-            self.labels.remove(label)
+            self.labels.remove(Labels[label_to_remove])
         except ValueError:
             return
