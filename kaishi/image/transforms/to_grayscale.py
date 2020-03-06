5da29bee1a11ff4e11f5647948b528a6783527c9
Michael Wharton <michael@kungfu.ai>
2020-03-06 13:37:19 -0600

    Add grayscale conversion transform

---
+++ kaishi/image/transforms/to_grayscale.py
@@ -0,0 +1,16 @@
+"""Transform image(s) to grayscale."""
+from kaishi.core.pipeline_component import PipelineComponent
+
+
+class TransformToGrayscale(PipelineComponent):
+    """Filter file list if non-image extensions exist."""
+
+    def __init__(self, dataset):
+        super().__init__(dataset)
+        self.applies_to_available = True
+        self.configure()
+
+    def __call__(self):
+        # Trim any files without image extensions
+        for i in self.get_target_indexes():
+            self.dataset.files[i].convert_to_grayscale()
