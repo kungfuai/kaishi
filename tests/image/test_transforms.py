77db7f28793e2fa4d457a0a685d87f2410d9cd6c
Michael Wharton <michael@kungfu.ai>
2020-03-08 08:18:29 -0500

    Add tests for limiting dimensions

--- tests/image/test_transforms.py
+++ tests/image/test_transforms.py
@@ -18,3 +18,48 @@ def test_fix_rotation():
         if fobj.has_label("UPSIDE_DOWN"):
             found_rotated = True
     assert found_rectified is True and found_rotated is False
+
+
+def test_limit_dimensions():
+    test = ImageFileGroup("tests/data/image", recursive=True)
+    test.configure_pipeline(["TransformLimitDimensions"])
+    test.pipeline.components[0].configure(max_dimension=100)
+    test.run_pipeline()
+    heights = [fobj.image.size[0] for fobj in test.files if fobj.image]
+    widths = [fobj.image.size[0] for fobj in test.files if fobj.image]
+    assert len(test.files) > 0
+    assert max([max(widths), max(heights)]) == 100
+
+
+def test_limit_dimensions_width_only():
+    test = ImageFileGroup("tests/data/image", recursive=True)
+    test.configure_pipeline(["TransformLimitDimensions"])
+    test.pipeline.components[0].configure(max_width=100)
+    test.run_pipeline()
+    widths = [fobj.image.size[0] for fobj in test.files if fobj.image]
+    assert len(test.files) > 0
+    assert max(widths) == 100
+
+
+def test_limit_dimensions_height_only():
+    test = ImageFileGroup("tests/data/image", recursive=True)
+    test.configure_pipeline(["TransformLimitDimensions"])
+    test.pipeline.components[0].configure(max_height=100)
+    test.run_pipeline()
+    heights = [fobj.image.size[1] for fobj in test.files if fobj.image]
+    assert len(test.files) > 0
+    assert max(heights) == 100
+
+
+def test_limit_dimensions_no_setting():
+    test = ImageFileGroup("tests/data/image", recursive=True)
+    test.configure_pipeline(["TransformLimitDimensions"])
+    test.load_all()
+    heights_before = [fobj.image.size[0] for fobj in test.files if fobj.image]
+    widths_before = [fobj.image.size[0] for fobj in test.files if fobj.image]
+    test.run_pipeline()
+    heights_after = [fobj.image.size[0] for fobj in test.files if fobj.image]
+    widths_after = [fobj.image.size[0] for fobj in test.files if fobj.image]
+    assert len(test.files) > 0
+    assert sum(heights_before) == sum(heights_after)
+    assert sum(widths_before) == sum(widths_after)
