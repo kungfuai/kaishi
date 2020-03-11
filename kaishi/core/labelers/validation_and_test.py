c73a52227b8693f60170177070a960b1cead9cdb
Michael Wharton <michael@kungfu.ai>
2020-03-11 15:44:57 -0500

    Add validation and test labeler

--- kaishi/core/labelers/validation_and_test.py
+++ kaishi/core/labelers/validation_and_test.py
@@ -22,13 +22,22 @@ class LabelerValidationAndTest(PipelineComponent):
         if self.seed is not None:
             random.seed(self.seed)
         val_ind = random.sample(all_ind, round(len(dataset.files) * self.val_frac))
-        _, remaining = trim_list_by_inds(all_ind, val_ind)
-        test_ind = random.sample(remaining, round(len(dataset.files) * self.test_frac))
+        remaining, _ = trim_list_by_inds(all_ind, val_ind)
+        test_relative_ind = random.sample(
+            list(range(len(remaining))), round(len(dataset.files) * self.test_frac)
+        )
+        test_ind = [remaining[i] for i in test_relative_ind]
+        train_relative_ind, _ = trim_list_by_inds(
+            list(range(len(remaining))), test_relative_ind
+        )
+        train_ind = [remaining[i] for i in train_relative_ind]
 
         for i in val_ind:
             dataset.files[i].add_label("VALIDATE")
         for i in test_ind:
             dataset.files[i].add_label("TEST")
+        for i in train_ind:
+            dataset.files[i].add_label("TRAIN")
 
     def configure(self, val_frac=0.2, test_frac=0.0, seed=None):
         """Default configuration returns false always."""
