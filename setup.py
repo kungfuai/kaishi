87d3b244792c98457d0d72815b35ef35b3a7f18a
Michael Wharton <michael@kungfu.ai>
2020-03-23 10:32:04 -0500

    Confiure for PyPI

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import os
+import pathlib
+import pkg_resources
 from setuptools import setup, find_packages
 
 
@@ -6,7 +8,20 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-print(find_packages())
+with pathlib.Path("requirements.txt").open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Libraries",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3.6",
+]
+
 setup(
     name="kaishi",
     version="0.0.1",
@@ -16,5 +31,8 @@ setup(
     url="https://github.com/kungfuai/kaishi",
     packages=find_packages(),
     include_package_data=True,
+    download_url="https://github.com/kungfuai/kaishi/archive/0.1.tar.gz",
+    classifiers=classifiers,
+    install_requires=install_requires,
     zip_safe=False,
 )
