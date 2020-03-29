e20360170ccac2111cab61fcd71b81be3c2a7468
Michael Wharton <michael@kungfu.ai>
2020-03-29 17:14:41 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -29,14 +29,14 @@ install_requires = [
 
 setup(
     name="kaishi",
-    version="0.11",
+    version="0.12",
     author="KUNGFU.AI",
     author_email="michael@kungfu.ai",
     description=("Tool kit to accelerate the first steps of the data science process"),
     url="https://github.com/kungfuai/kaishi",
     packages=find_packages(),
     include_package_data=True,
-    download_url="https://github.com/kungfuai/kaishi/archive/0.11.tar.gz",
+    download_url="https://github.com/kungfuai/kaishi/archive/0.12.tar.gz",
     install_requires=install_requires,
     classifiers=classifiers,
     zip_safe=False,
